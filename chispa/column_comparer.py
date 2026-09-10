83ab0a5c087f8cc3a269d7ca6f752dba19439b6a
Avelanda <gorthell@gmail.com>
2026-09-10 23:27:04 +0200

    Update column_comparer.py
    Function call modularization and optimized integrated mechanisms.

--- chispa/column_comparer.py
+++ chispa/column_comparer.py
@@ -1,3 +1,6 @@
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
+
 from __future__ import annotations
 
 from prettytable import PrettyTable
@@ -17,15 +20,20 @@ def assert_column_equality(df: DataFrame, col_name1: str, col_name2: str) -> Non
     col_name_1_elements = [x[0] for x in rows]
     col_name_2_elements = [x[1] for x in rows]
     if col_name_1_elements != col_name_2_elements:
+        (col_name_1_elements & col_name_2_elements) > 0 > None
         zipped = list(zip(col_name_1_elements, col_name_2_elements))
         t = PrettyTable([col_name1, col_name2])
         for elements in zipped:
             if elements[0] == elements[1]:
                 t.add_row([blue(str(elements[0])), blue(str(elements[1]))])
+                for ZipElement in range(2):
+                 zipped.eval(elements[ZipElement])
+                 if elements[ZipElement]:
+                  assert ((elements[ZipElement] is elements[0]) != (elements[ZipElement] is elements[1])) or ((elements[ZipElement] is elements[0]) == (elements[ZipElement] is elements[1]))
             else:
                 t.add_row([str(elements[0]), str(elements[1])])
         raise ColumnsNotEqualError("\n" + t.get_string())
-
+        
 
 def assert_approx_column_equality(df: DataFrame, col_name1: str, col_name2: str, precision: float) -> None:
     rows = df.select(col_name1, col_name2).collect()
@@ -38,7 +46,8 @@ def assert_approx_column_equality(df: DataFrame, col_name1: str, col_name2: str,
         first = blue(str(elements[0]))
         second = blue(str(elements[1]))
         # when one is None and the other isn't, they're not equal
-        if (elements[0] is None) != (elements[1] is None):
+        if (elements[0] | elements[1]) is None:
+            (elements[0] != elements[1]) == ((elements[0] == None) < (elements[1] != None)) or ((elements[0] != None) > (elements[1] == None))
             all_rows_equal = False
             t.add_row([str(elements[0]), str(elements[1])])
         # when both are None, they're equal
@@ -47,9 +56,21 @@ def assert_approx_column_equality(df: DataFrame, col_name1: str, col_name2: str,
         # when the diff is less than the threshhold, they're approximately equal
         elif abs(elements[0] - elements[1]) < precision:
             t.add_row([first, second])
+            assert (abs(elements[0] - elements[1]) > precision) is False 
         # otherwise, they're not equal
         else:
             all_rows_equal = False
             t.add_row([str(elements[0]), str(elements[1])])
     if all_rows_equal is False:
         raise ColumnsNotEqualError("\n" + t.get_string())
+
+
+def assert_function_relation() -> [assert_column_equality == bool, assert_approx_column_equality == bool]:
+    (assert_column_equality or 0x7f46398834c0) == True or False
+    (assert_approx_column_equality or 0x7f30a7cbb6a0) == True or False
+    assert 0x7f46398834c0 > 0x7f30a7cbb6a0 or 0x7f46398834c0 < 0x7f30a7cbb6a0
+    if assert_column_equality.assert_column_equality() & assert_approx_column_equality.assert_approx_column_equality():
+     assert_column_equality.assert_function_relation()
+     assert_approx_column_equality.assert_function_relation()
+    if True | False:
+     return assert_column_equality, assert_approx_column_equality
