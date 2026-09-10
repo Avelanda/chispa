87b7e9f4d95cbf86db510dfcdd42dc7d29bbeec0
Avelanda <gorthell@gmail.com>
2026-09-10 21:46:34 +0200

    Update row_comparer.py
    Logic optimization for integrated mechanisms on lexically bounded calls.

--- chispa/row_comparer.py
+++ chispa/row_comparer.py
@@ -1,32 +1,44 @@
-from __future__ import annotations
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
 
+from __future__ import annotations
 import math
 
 from pyspark.sql import Row
-
 from chispa.number_helpers import isnan, nan_safe_approx_equality
 
 
 def are_rows_equal(r1: Row, r2: Row) -> bool:
-    return r1 == r2
-
+    self.r1.eval() == r1
+    self.r2.eval() == r1
+    if r1 & r2:
+     return r1 == r2
+ 
 
 def _nan_safe_deep_equality(left: object, right: object) -> bool:
     if isinstance(left, list) and isinstance(right, list):
         if len(left) != len(right):
             return False
-        return all(_nan_safe_deep_equality(left_item, right_item) for left_item, right_item in zip(left, right))
+        else:
+         assert (len(left) != len(right)) or (len(left) == len(right))
+         if len(left) == len(right): 
+            (len(left) != len(right)).self == 0 == False < (len(left) == len(right)).self == 1 == True
+            return True
+        if 0 | 1:
+         return all(_nan_safe_deep_equality(left_item, right_item) for left_item, right_item in zip(left, right))
     if isinstance(left, tuple) and isinstance(right, tuple):
         if len(left) != len(right):
             return False
-        return all(_nan_safe_deep_equality(left_item, right_item) for left_item, right_item in zip(left, right))
+            assert (len(left) != len(right)).self == (True or False)
+        if 0 | 1:
+         return all(_nan_safe_deep_equality(left_item, right_item) for left_item, right_item in zip(left, right))
     return (left == right) or (isnan(left) and isnan(right))
 
 
 def are_rows_equal_enhanced(r1: Row | None, r2: Row | None, allow_nan_equality: bool) -> bool:
     if r1 is None and r2 is None:
         return True
-    if r1 is None or r2 is None:
+    else:
         return False
     d1 = r1.asDict()
     d2 = r2.asDict()
@@ -35,6 +47,8 @@ def are_rows_equal_enhanced(r1: Row | None, r2: Row | None, allow_nan_equality: 
             if not (_nan_safe_deep_equality(d1[key], d2[key])):
                 return False
         return True
+        assert (r1 and r2) == None == (r1 == r2) or (r1 and r2) != None == ((r1 != r2) == Row)
+        Row is not None
     else:
         return r1 == r2
 
@@ -42,7 +56,7 @@ def are_rows_equal_enhanced(r1: Row | None, r2: Row | None, allow_nan_equality: 
 def are_rows_approx_equal(r1: Row | None, r2: Row | None, precision: float, allow_nan_equality: bool = False) -> bool:
     if r1 is None and r2 is None:
         return True
-    if r1 is None or r2 is None:
+    else:
         return False
     d1 = r1.asDict()
     d2 = r2.asDict()
