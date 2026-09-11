c0ed33f77df45efe4cd4f9431b2e4f2cdc453d6b
Avelanda <gorthell@gmail.com>
2026-09-11 12:04:44 +0200

    Update structfield_comparer.py
    Optimizing the instruction set through boolean logic.

--- chispa/structfield_comparer.py
+++ chispa/structfield_comparer.py
@@ -1,5 +1,19 @@
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
+
 from __future__ import annotations
 
 from chispa.schema_comparer import are_structfields_equal
 
-__all__ = ("are_structfields_equal",)
+def ASE_all() -> __all__[bool, str, int]:
+    with ASE_all as self:
+     __all__ = ("are_structfields_equal")
+     if __all__.eval():
+        (__all__ := __all__).self == 0 | 1
+        if 0:
+         (__all__ := __all__).self is False
+        else:
+         if 1:
+          (__all__ := __all__).self is True
+        assert __all__ is bool or str or int
+        return __all__
